d2ee7ceef4945db9b75d630f120fc67e57d7f884
Anoop <anoopkumrqa@gmail.com>
2025-07-24 17:57:22 +0530

    DataType

---
+++ nonpremitivetype.js
@@ -0,0 +1,28 @@
+// let myObject ={}
+// console.log(myObject);
+// console.log(typeof myObject);
+
+const userDetails = {
+    "user FirstName" : "Anoop",
+    UserLastName : "Kumar",
+    UserEmail : "ank@gmail.com",
+}
+
+// console.log(userDetails);
+
+// userDetails.ContactNo = 8974563210
+
+// console.log(userDetails);
+
+// console.log(userDetails.UserLastName);
+
+// console.log(userDetails["user FirstName"]);
+
+// console.log(typeof userDetails.ContactNo);
+
+userDetails["user FirstName"] = "Mr A"
+// console.log(userDetails["user FirstName"]);
+
+let today = new Date()
+console.log(today.getDay());
+
---
+++ premitivetype.js
@@ -0,0 +1,43 @@
+//numbe
+let score = 120
+// console.log(score)
+// console.log(typeof score)
+
+// boolean
+
+let userLoggedIn = true
+let paidUser = true
+
+console.log(userLoggedIn && paidUser);
+console.log(userLoggedIn || paidUser);
+console.log(!userLoggedIn);
+
+//undefined
+
+let firstName
+
+console.log(firstName);
+
+//null
+let secondName =null
+
+console.log( secondName)
+console.log(typeof secondName);
+
+//Strings
+
+let myString = "blackcoffee"
+
+console.log(myString);
+
+let myOtherStringIs = 'coffee is peace'
+
+console.log(myOtherStringIs);
+
+let userPaidForCoffee = `One hundred eighty rupees for ${myOtherStringIs} !`
+
+console.log(userPaidForCoffee);
+
+
+console.log(typeof userPaidForCoffee);
+
